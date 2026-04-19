07da833836a0c7e917f95b4e382cade6052bfbdc
Chris Umali <chrisumali.work@gmail.com>
2026-04-19 14:54:50 +0800

    Initial create

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  plugins: ["react"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "no-unused-vars": "warn",
+    "react/react-in-jsx-scope": "off"
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  const myval = 123;
+  
+  return (
+    <h1>React Investment Calculator</h1>
+  )
+}
+
+export default App
